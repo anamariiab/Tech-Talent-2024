858c31a113c3ed5b283ec1f3ecc18d529031031f
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-07 23:35:21 +0100

    Mi primer commit

--- C1_Introducción_A_La_Programación/T2JavaIde/src/T2Ejercicio01App.java
+++ C1_Introducción_A_La_Programación/T2JavaIde/src/T2Ejercicio01App.java
@@ -2,7 +2,10 @@
 public class T2Ejercicio01App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");
 
 	}
 
