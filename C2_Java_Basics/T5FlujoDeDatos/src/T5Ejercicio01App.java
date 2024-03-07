644063046e30c42859e4a9c67b7f327d919cd7fe
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-08 00:12:29 +0100

    T5Ejercicio01App

---
+++ C2_Java_Basics/T5FlujoDeDatos/src/T5Ejercicio01App.java
@@ -0,0 +1,62 @@
+
+public class T5Ejercicio01App {
+
+	public static void main(String[] args) {
+		
+		// Declara 2 variables numéricas (con el valor que desees)
+		int numero1 = 12;
+		int numero2 = 14;
+
+		// Indica cual es mayor de los dos. Si son iguales indicarlo también
+		if (numero1 > numero2) {
+
+			System.out.println("El número 1 es mayor que el número 2.");
+
+		} else if (numero1 < numero2) {
+
+			System.out.println("El número 2 es mayor que el número 1.");
+
+		} else {
+
+			System.out.println("Ambos números son iguales.");
+		}
+
+		// Declara 2 variables numéricas (con el valor que desees)
+		int a = 10;
+		int b = 10;
+
+		// Indica cual es mayor de los dos. Si son iguales indicarlo también
+		if (a > b) {
+
+			System.out.println("El a es mayor que el b.");
+
+		} else if (a < b) {
+
+			System.out.println("El a es mayor que el b.");
+
+		} else {
+
+			System.out.println("Ambos son iguales.");
+		}
+
+		// Declara 2 variables numéricas (con el valor que desees)
+		int habitacion1 = 9;
+		int habitacion2 = 7;
+
+		// Indica cual es mayor de los dos. Si son iguales indicarlo también.
+		if (habitacion1 > habitacion2) {
+
+			System.out.println("La habitación 1 es mas grande que la habitación 2.");
+
+		} else if (habitacion1 < habitacion2) {
+
+			System.out.println("La habitación 2 es mas grande que la habitación 1.");
+
+		} else {
+
+			System.out.println("Ambas son iguales.");
+		}
+
+	}
+
+}
