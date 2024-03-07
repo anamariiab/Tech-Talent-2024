cf091235b7f89144db9ffee1c7d72af61def05b2
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-08 00:08:57 +0100

    T4Ejercicio04

---
+++ C2_Java_Basics/T4PruebaApp/src/T4Ejercicio04App.java
@@ -0,0 +1,27 @@
+
+public class T4Ejercicio04App {
+
+	public static void main(String[] args) {
+		// Declarar una variable entera N
+				int N;
+				
+				// Asigna un valor
+				N = 5;
+				
+				System.out.println(N);
+			   
+				N+=77;
+				
+				System.out.println(N);
+				
+				N-=3;
+				
+				System.out.println(N);
+				
+				N*=2;
+				
+				System.out.println(N);
+
+			}
+
+		}
