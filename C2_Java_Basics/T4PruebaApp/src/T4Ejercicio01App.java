28235adb7dcf9943b7886ae1b7a0096baf72d78a
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-08 00:05:12 +0100

    T4Ejercicio01

---
+++ C2_Java_Basics/T4PruebaApp/src/T4Ejercicio01App.java
@@ -0,0 +1,31 @@
+
+public class T4Ejercicio01App {
+
+	public static void main(String[] args) {
+
+	    // Declara dos variables numéricas (con el valor que desees)
+		int numero1 = 15;
+		int numero2 = 20;
+		
+		// Muestra por consola la suma
+		int suma = numero1+numero2;
+		System.out.println(suma);
+		
+		// Muestra por consola la resta
+		int resta = numero1-numero2;
+		System.out.println(resta);		
+		
+	    // Muestra por consola la multiplicación
+		int multiplicacion = numero1*numero2;
+		System.out.println(multiplicacion);
+		
+		// Muestra por consola la división
+		int division = numero1/numero2;
+		System.out.println(division);
+		
+		// Muestra por consola el modulo
+		int modulo = numero1%numero2;
+		System.out.println(modulo);
+	}
+
+}
