ea2c328fd7a04fc3eb5e45ab1d93dd819451605a
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-08 12:32:27 +0100

    T5Ejercicio04

---
+++ C2_Java_Basics/T5FlujoDeDatos/src/T5Ejercicio04App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class T5Ejercicio04App {
+
+	public static void main(String[] args) {
+		//Haz una aplicación que calcule el área de un circulo (pi*R2). El radio se pedirá por teclado (recuerda pasar de String a double con Double.parseDouble). Usa la constante PI y elmétodo pow de Math.
+				Scanner sc = new Scanner(System.in);
+				
+				System.out.println("Porfavor, introduce el radio del círculo: ");
+				
+				String introRADIO = sc.nextLine();
+				
+				sc.close();
+				
+				double radio = Double.parseDouble(introRADIO);
+				double area = Math.PI * Math.pow(radio, 2);
+				
+				System.out.println("El área del círculo con radio " + introRADIO + " es " + area );
+				
+			}
+
+		}
