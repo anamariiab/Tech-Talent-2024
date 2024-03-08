c1cd0af4aed621f61cad0c06cee89dd84965c7f2
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-08 13:29:54 +0100

    T5Ejercicio5

---
+++ C2_Java_Basics/T5FlujoDeDatos/src/T5Ejercicio05App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class T5Ejercicio05App {
+
+	public static void main(String[] args) {
+		
+		// Lee un número por teclado e indica si es divisible entre 2.
+				Scanner sc = new Scanner(System.in);
+				
+				    System.out.println("Porfavor, introduce un numero entero:");
+				
+				String introNUM = sc.nextLine();
+			
+				sc.close();
+				
+				// Indica si es divisible entre 2 (resto = 0). Si no lo es, también debemos indicarlo.
+		        double num = Double.parseDouble(introNUM);
+				if (num % 2 == 0) {
+		            System.out.println(num + " es divisible por 2.");
+		        } else {
+		            System.out.println(num + " no es divisible por 2.");
+		        }
+
+
+			}
+}
