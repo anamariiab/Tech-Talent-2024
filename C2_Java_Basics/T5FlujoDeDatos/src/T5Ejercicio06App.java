93e705fd48f474f87265079db6fe84e9943e671e
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-08 13:31:09 +0100

    T5Ejercicio06

---
+++ C2_Java_Basics/T5FlujoDeDatos/src/T5Ejercicio06App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class T5Ejercicio06App {
+
+	public static void main(String[] args) {
+		
+		// Lee un número por teclado que pida el precio de un producto (puede tener decimales)
+		Scanner sc = new Scanner(System.in);
+		
+		    System.out.println("Añade el precio del producto: ");
+		
+		String initialPROD = sc.nextLine();
+		
+		sc.close();
+		
+		// Calcula el precio final con IVA 21%
+		double prod = Double.parseDouble(initialPROD);
+		final double IVA = 0.21;
+		double preciofinal = prod*(1+IVA);
+		
+		    System.out.println ("El precio final con IVA es: "+ preciofinal);
+		
+
+	}
+
+}
