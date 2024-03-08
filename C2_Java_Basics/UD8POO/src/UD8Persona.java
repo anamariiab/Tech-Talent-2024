410335d84dacd8f545512fa465c3827f5d9467e7
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-08 12:44:58 +0100

    UD8Persona

---
+++ C2_Java_Basics/UD8POO/src/UD8Persona.java
@@ -0,0 +1,54 @@
+
+public class UD8Persona {
+		
+		// Atributos (o campos/propiedades)
+	    private String nombre;
+	    private int edad;
+	    private String ciudad;
+
+	    // Constructor(es)
+	    public UD8Persona() {
+	        // Constructor por defecto
+	    }
+
+	    public UD8Persona(String nombre, int edad, String ciudad) {
+	        this.nombre = nombre;
+	        this.edad = edad;
+	        this.ciudad = ciudad;
+	    }
+
+	    // Métodos (o funciones)
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    public int getEdad() {
+	        return edad;
+	    }
+
+	    public void setEdad(int edad) {
+	        this.edad = edad;
+	    }
+
+	    public String getCiudad() {
+	        return ciudad;
+	    }
+
+	    public void setCiudad(String ciudad) {
+	        this.ciudad = ciudad;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Persona{" +
+	                "nombre='" + nombre + '\'' +
+	                ", edad=" + edad +
+	                ", ciudad='" + ciudad + '\'' +
+	                '}';
+	    }
+
+}
