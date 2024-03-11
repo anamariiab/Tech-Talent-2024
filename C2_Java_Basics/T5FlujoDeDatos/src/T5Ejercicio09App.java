95efcd9a9308302d20c186166444cf547cb48b1a
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-11 13:20:52 +0100

    T5Ejercicio09

---
+++ C2_Java_Basics/T5FlujoDeDatos/src/T5Ejercicio09App.java
@@ -0,0 +1,18 @@
+
+public class T5Ejercicio09App {
+
+	public static void main(String[] args) {
+		// Muestra los números del 1 al 100 (ambos incluidos) divisibles entre 2 y 3.
+		// Utiliza el bucle que desees.
+
+		for (int num = 1; num <= 100; num++) {
+			if (num % 2 == 0 && num % 3 == 0) {
+
+				System.out.println(num);
+			}
+
+		}
+
+	}
+
+}
