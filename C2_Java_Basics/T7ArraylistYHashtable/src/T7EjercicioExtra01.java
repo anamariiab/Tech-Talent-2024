f3937531884b9de2030aed15dcc7f070ead09bdd
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-13 23:45:29 +0100

    comentarios T7 Eje extra 01

--- C2_Java_Basics/T7ArraylistYHashtable/src/T7EjercicioExtra01.java
+++ C2_Java_Basics/T7ArraylistYHashtable/src/T7EjercicioExtra01.java
@@ -12,56 +12,60 @@ public class T7EjercicioExtra01 {
 
 		Map<String, ArrayList<Integer>> estudiantesUniversidad = new HashMap<>();
 
+		System.out.println();
 		System.out.print("Por favor, introduce el número total de estudiantes: ");
 		int numTotalEstudiantes = sc.nextInt();
+		System.out.println("=======================================================\n");
 		
-		inputNombre(estudiantesUniversidad, numTotalEstudiantes);
+		inputNombre(estudiantesUniversidad, numTotalEstudiantes);                                                                      //se llama al met que solicita y almacena info sobre cada estudiante
 	    mostrarInfoEstudiantes(estudiantesUniversidad);
 	}
 
 	public static void inputNombre(Map<String, ArrayList<Integer>> estudiantesUniversidad, int numeroEstudiantes) {
 
 		for (int i = 0; i < numeroEstudiantes; i++) {
-			System.out.print("Por favor, introduce el nombre del estudiante " + (i + 1) + ": ");
+			System.out.print("Por favor, introduce el nombre del estudiante: " + (i + 1) + ": ");
 			String nombreEstudiante = sc.next();
 
-			ArrayList<Integer> notas = obtenerNotas();
-			estudiantesUniversidad.put(nombreEstudiante, notas);
+			ArrayList<Integer> calificaciones = ingresarCalificaciones();                                                                         //se llama al met para obtener las calificaciones
+			estudiantesUniversidad.put(nombreEstudiante, calificaciones);
 		}
 	}
 
-	public static ArrayList<Integer> obtenerNotas() {
+	public static ArrayList<Integer> ingresarCalificaciones() {
 
-		ArrayList<Integer> notas = new ArrayList<>();
+		ArrayList<Integer> calificaciones = new ArrayList<>();
 
-		boolean continuar = true;
-		while (continuar) {
-			System.out.print("Por favor, introduce las notas (o '0' para finalizar: ");
-			int inputNotas = sc.nextInt();
+		boolean continuar = true;                                                                                      //var booleana para determinar si el user desea seguir agregando calif o finalizar el input
+		while (continuar) {                                                                                            //se inicialia siempre que la boolean sea true
+			System.out.print("Por favor, introduce las calificaciones (o '0' para finalizar: ");
+			int inputCalificaciones = sc.nextInt();                                                                  //solicitamos calificaciones y las almacenamos en un arraylist
+			System.out.println("\n");
 
-			if (inputNotas == 0) {
+			if (inputCalificaciones == 0) {                                                                               //si el valor ingresado es 0 se establece la boolean en false y se finaliza la entrada
 				continuar = false;
 			} else {
-				notas.add(inputNotas);
+				calificaciones.add(inputCalificaciones);                                                                  
 			}
 		}
 
-		return notas;
+		return calificaciones;
 	}
 
 	public static void mostrarInfoEstudiantes(Map<String, ArrayList<Integer>> estudiantesUniversidad) {
 
 		for (String nombreEstudiante : estudiantesUniversidad.keySet()) {
-			ArrayList<Integer> notas = estudiantesUniversidad.get(nombreEstudiante);
+			ArrayList<Integer> calificaciones = estudiantesUniversidad.get(nombreEstudiante);                          //para cada nombre obtiene la lista de calificaciones asociada a ese estudiante
 
-			double promedio = calcularPromedio(notas);
+			double promedio = calcularPromedio(calificaciones);                                                        //llamamos al metodo calcularPromedio pasando la lista como argumento y almacena el resultado en la var
 
+			System.out.println("=================================================================\n");
 			System.out.println("Estudiante: " + nombreEstudiante);
-			System.out.println("Notas: " + notas);
+			System.out.println("Calificaciones: " + calificaciones);                                     
 			System.out.println("Promedio: " + promedio);
 
 			if (promedio >= 60) {
-				System.out.println("Aprobado!");
+				System.out.println("Aprobado!");                                                                            //determina si el estudiante ha aprob o no
 			} else {
 				System.out.println("Suspendido!");
 			}
@@ -72,12 +76,12 @@ public class T7EjercicioExtra01 {
 
 	}
 
-	public static double calcularPromedio(ArrayList<Integer> notas) {
+	public static double calcularPromedio(ArrayList<Integer> calificaciones) {
 		
 		int suma = 0;
-        for (int nota : notas) {
-            suma += nota;
+        for (int calificacion : calificaciones) {
+            suma += calificacion;                                                                                                  //cada calificacion se suma a la var suma
         }
-        return suma / (double) notas.size();
+        return suma / (double) calificaciones.size();                                                                              //double para garantizar que el resultado sea nr decimal
     }
 }
