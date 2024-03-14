0e5251676f365dc2c9a712c49f8d3d78354e2beb
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-15 00:21:59 +0100

    T8 Ej2 empez

---
+++ C2_Java_Basics/T8POO/src/Password.java
@@ -0,0 +1,21 @@
+
+public class Password {
+
+	private String contrasenya;
+	private int longitud;
+	
+
+	public Password() {
+
+	//	this.contrasenya = "  ";
+		this.longitud = 8;
+	}
+
+	
+	
+	
+	public static void main(String[] args) {
+
+	}
+
+}
