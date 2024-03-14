12e0e8ef0f08b4f61b806fcbc35ae0aca4cc3ea9
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-14 13:18:45 +0100

    T8 Eje1 empez

--- C2_Java_Basics/T8POO/src/Persona.java
+++ C2_Java_Basics/T8POO/src/Persona.java
@@ -1,69 +1,109 @@
 
 public class Persona {
+	
+    private String nombre;
+    private int edad;
+    private String DNI;                                                       //atributos
+    private String sexo;
+    private double peso;
+    private double altura;
 
-	private String nombre;
-	private int edad;
-//	private final String DNI;                              //atributos
-//	private char sexo;
-	private int peso;
-	private double altura;
-
-
-	public Persona() {
-		
-        this.nombre = " ";
-        this.edad = 0;                               //constructor por defecto
- //     this.DNI = "";
- //     this.sexo = "";
-        this.peso = 0;
-        this.altura = 0;
+    public Persona() {
+    	
+        this.nombre = "  ";
+        this.edad = 0;
+        this.DNI = "  ";
+        this.sexo = " ";                                                // constructor por defecto
+        this.peso = 0.0;
+        this.altura = 0.0;
     }
-	
-	
-	public Persona(String nombre, int edad, char sexo) {
-		
+
+    public Persona(String nombre, int edad, String hombre) {
+    	
         this.nombre = nombre;
         this.edad = edad;
-  //    this.DNI = generaDNI();                            //constructor sin pasar dni, peso y altura
- //     this.sexo = sexo;
-        this.peso = 0;
-        this.altura = 0;
+        this.DNI = "  ";                                                  // constructor con nombre, edad, sexo
+        this.sexo = hombre;
+        this.peso = 0.0;
+        this.altura = 0.0;
     }
-	
-	
-	public Persona(String nombre, int edad, String DNI, char sexo, double peso, double altura) {
-		
+
+    public Persona(String nombre, int edad, String DNI, String hombre, double peso, double altura) {
+    	
         this.nombre = nombre;
         this.edad = edad;
-  //    this.DNI = generaDNI();                            //constructor con todos los atributos 
- //     this.sexo = sexo;
+        this.DNI = DNI;                                                       
+        this.sexo = hombre;
         this.peso = peso;
         this.altura = altura;
     }
-	
-	
-	public String getNombre() {
+
+    public String getNombre() {
+    	
         return nombre;
     }
-    
+
+    public void setNombre(String nombre) {
+    	
+        this.nombre = nombre;
+    }
+
     public int getEdad() {
+    	
         return edad;
     }
-    
- // public String getDNI() {
-//  return DNI;
-//  }
-    
-//  public char getSexo() {
-//  return sexo;
-//  }
-    
+
+    public void setEdad(int edad) {
+    	
+        this.edad = edad;
+    }
+
+    public String getDNI() {
+    	
+        return DNI;
+    }
+
+    public String getSexo() {
+    	
+        return sexo;
+    }
+
+    public void setSexo(String sexo) { 
+
+        this.sexo = sexo;
+    }
+
     public double getPeso() {
+    	
         return peso;
     }
-    
+
+    public void setPeso(double peso) {
+    	
+        this.peso = peso;
+    }
+
     public double getAltura() {
+    	
         return altura;
     }
+
+    public void setAltura(double altura) {
+    	
+        this.altura = altura;
+    }
     
+    
+    
+    public static void main(String[] args) {
+    	
+    	Persona personaA = new Persona("Laura", 29, "X");
+    	Persona personaB = new Persona("Victor", 20, "X");
+    	
+    	System.out.println(personaA.getNombre()); 
+		System.out.println(personaA.getEdad()); 
+		System.out.println(personaA.getDNI());
+    	
+    }
 }
+
