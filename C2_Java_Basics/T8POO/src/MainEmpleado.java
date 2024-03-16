cc33f1d76f28a40fee35a92dab7c4f38233312e1
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-16 21:43:27 +0100

    T8 Ejercicios Extra 1

---
+++ C2_Java_Basics/T8POO/src/MainEmpleado.java
@@ -0,0 +1,19 @@
+
+public class MainEmpleado {
+
+	public static void main(String[] args) {
+
+		Empleado empleadoA = new Empleado("Javier", 30, 1500.0);
+		Empleado empleadoB = new Empleado("Emilia", 22, 1700.0);
+
+		System.out.println("===============================");
+		System.out.println("··Datos del empleado A··");
+		System.out.println(empleadoA.toString());
+		System.out.println("Salario anual: €" + empleadoA.calculoSalarioAnual());
+
+		System.out.println("===============================");
+		System.out.println("··Datos del empleado B··");
+		System.out.println(empleadoB.toString());
+		System.out.println("Salario anual: €" + empleadoB.calculoSalarioAnual());
+	}
+}
