1d4f305908f6b542a3baf5e0fdc12dc5e34371a5
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-16 15:22:40 +0100

    Eje2 T8

--- C2_Java_Basics/T8POO/src/MainPassword.java
+++ C2_Java_Basics/T8POO/src/MainPassword.java
@@ -2,8 +2,11 @@
 public class MainPassword {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Password constrasenya1 = new Password();
+		Password constrasenya2 = new Password(12);
+
+		System.out.println("··Contraseña 1: " + constrasenya1.getContrasenya());
+		System.out.println("··Contraseña 2: " + constrasenya2.getContrasenya());
 	}
-
 }
--- C2_Java_Basics/T8POO/src/Password.java
+++ C2_Java_Basics/T8POO/src/Password.java
@@ -1,21 +1,50 @@
+import java.util.Random;
 
 public class Password {
 
 	private String contrasenya;
 	private int longitud;
 	
-
+	
 	public Password() {
-
-	//	this.contrasenya = "  ";
+		
+		this.contrasenya = generarConstrasenya(longitud);
 		this.longitud = 8;
 	}
 
-	
-	
-	
-	public static void main(String[] args) {
-
+	public Password(int longitud) {
+		
+		this.contrasenya = generarConstrasenya(longitud);
+		this.longitud = longitud;
 	}
 
+	public String getContrasenya() {
+		return contrasenya;
+	}
+
+	public void setContrasenya(String contrasenya) {
+		this.contrasenya = contrasenya;
+	}
+
+	public int getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(int longitud) {
+		this.longitud = longitud;
+	}
+
+	
+	private String generarConstrasenya(int longitud) {
+		
+		Random random = new Random();
+		StringBuilder constrasenyaGenerada = new StringBuilder();
+
+		for (int i = 0; i < longitud; i++) {
+			int num = random.nextInt(9);
+			constrasenyaGenerada.append(num);
+		}
+
+		return constrasenyaGenerada.toString();
+	}
 }
