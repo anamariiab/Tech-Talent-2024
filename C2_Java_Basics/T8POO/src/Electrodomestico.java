64312c26e5a47ed9840394e2df78b78b96eebd51
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-16 23:34:36 +0100

    .

--- C2_Java_Basics/T8POO/src/Electrodomestico.java
+++ C2_Java_Basics/T8POO/src/Electrodomestico.java
@@ -1,39 +1,48 @@
 
 public class Electrodomestico {
-	
-	private double precioBase; 
-	private String color; 
-	private String consumoEnergetico; 
-	private double peso; 
 
+	private double precioBase;
+	private String color;
+	private char consumoEnergetico;
+	private double peso;
 
-	
-	
-	
-	
-	
-	
-	
-	
+	public enum Color {
+		BLANCO, NEGRO, ROJO, AZUL, GRIS
+	}
+
+	public Electrodomestico() {
+
+		this.color = "blanco";
+		this.consumoEnergetico = 'F';
+		this.precioBase = 100;
+		this.peso = 5;
+	}
+
+	public Electrodomestico(double precioBase, double peso) {
+		
+		this.color = "blanco";
+		this.consumoEnergetico = 'F';
+		this.precioBase = precioBase;
+		this.peso = peso;
+	}
+
+	public Electrodomestico(double precioBase, double peso, String color, char consumoEnergetico) {
+		
+		this.color = color;
+		this.consumoEnergetico = consumoEnergetico;
+		this.precioBase = precioBase;
+		this.peso = peso;
+	}
+
+	private boolean validarConsumoEnergetico(char consumoEnergetico) {
+        return consumoEnergetico >= 'A' && consumoEnergetico <= 'F';
+    }
 	
 
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	   @Override
-	    public String toString() {
-	        return "Precio: " + precioBase + "Color: " + color + "Consumo energetico: " + consumoEnergetico + "Peso: " + peso;
-	    }	
+	@Override
+	public String toString() {
+		return "Precio: " + precioBase + "Color: " + color + "Consumo energetico: " + consumoEnergetico + "Peso: "
+				+ peso;
+	}
 
 }
