2435a1adf65a9b2890d60f4ab4534c1135bc5b67
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-24 21:55:12 +0100

    T9 eje 4 empez

---
+++ C2_Java_Basics/T9Herencia/src/Raices.java
@@ -0,0 +1,52 @@
+
+public class Raices {
+	
+	protected double a;
+	protected double b;
+	protected double c;
+	
+	public Raices() {
+		this.a = 0.0;
+		this.b = 0.0;
+		this.c = 0.0;
+	}
+	
+	public Raices(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	public double getDiscriminante () {
+		return a;
+		
+		
+	}
+
+	public boolean tieneRaices () {
+		return false;
+		
+		
+		
+	}
+	
+	public boolean tieneRaiz () {
+		return false;
+		
+		
+		
+	}
+	
+	public void calcular () {
+		
+		
+	}
+	
+	public void obtenerRaices() {
+	
+	}
+	
+	public void obtenerRaiz() {
+		
+	}
+}
