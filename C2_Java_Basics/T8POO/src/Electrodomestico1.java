e709f02990aa91c97f8c6514004e214c69da864f
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-25 09:31:32 +0100

    cambio nombre clases enum

--- C2_Java_Basics/T8POO/src/Electrodomestico1.java
+++ C2_Java_Basics/T8POO/src/Electrodomestico1.java
@@ -1,20 +1,20 @@
 public class Electrodomestico1 {
 
 	private double precioBase;
-	private Color color;                                                        //el atributo es de tipo Color. See below enumeracion color
-	private Consumo consumoEnergetico;                                          //el atributo es de tipo Consumo. See below enumeracion consumo
+	private ColorEnum color;                                                        //el atributo es de tipo Color. See below enumeracion color
+	private ConsumoEnum consumoEnergetico;                                          //el atributo es de tipo Consumo. See below enumeracion consumo
 	private double peso;
 	
 	private final static double PRECIO_BASE_PRED = 100;
-	private final static Color COLOR_PRED = Color.BLANCO;
-	private final static Consumo CONSUMO_ENERGETICO_PRED = Consumo.F;
+	private final static ColorEnum COLOR_PRED = ColorEnum.BLANCO;
+	private final static ConsumoEnum CONSUMO_ENERGETICO_PRED = ConsumoEnum.F;
 	private final static double PESO_PRED = 5;
 
-	public enum Color {                                                
+	public enum ColorEnum {                                                
 		BLANCO, NEGRO, ROJO, AZUL, GRIS                                     //enumeracion de constantes colores. solo puede tomar los valores de la enumeracion.  
 	}                                                                       //!!la enumeracion es muy util cuando hay que representar un conjunto FIJO de constantes!!
 	                                                                        //!!also, el ENUM detecta errores al intentar ingresar un valor que no esta definido en la enumeracion.
-	public enum Consumo{                                                    //enumeracion de constantes consumo. solo puede tomar los valores de la enumeracion.
+	public enum ConsumoEnum{                                                //enumeracion de constantes consumo. solo puede tomar los valores de la enumeracion.
 		A, B, C, D, E, F                                                    //!!also, claridad en el codigo, mas legible
 	}                                                                
 	
@@ -23,8 +23,8 @@ public class Electrodomestico1 {
 
 		this.color = COLOR_PRED;
 		this.consumoEnergetico = CONSUMO_ENERGETICO_PRED;                           //constr por defecto que inicializa los atributos con los valores pred
-		this.precioBase = 100;
-		this.peso = 5;
+		this.precioBase = PRECIO_BASE_PRED;
+		this.peso = PESO_PRED;
 		
 	}
 
@@ -37,7 +37,7 @@ public class Electrodomestico1 {
 
 	}
 
-	public Electrodomestico1(double precioBase, double peso, Color color, Consumo consumoEnergetico) {
+	public Electrodomestico1(double precioBase, double peso, ColorEnum color, ConsumoEnum consumoEnergetico) {
 		
 		this.color = color;
 		this.consumoEnergetico = consumoEnergetico;                                 //constr con todos los parametros
