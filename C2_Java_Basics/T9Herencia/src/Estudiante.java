26b7accc3a568f32d5d71829af0ecffb2512eab5
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-25 20:42:44 +0100

    T9 ej 5 empez

---
+++ C2_Java_Basics/T9Herencia/src/Estudiante.java
@@ -0,0 +1,18 @@
+
+public class Estudiante {
+	
+	protected String nombre;
+	protected int edad;
+	protected String sexo;
+	protected int calificacion;
+	
+	public Estudiante(String nombre, int edad, String sexo, int calificacion) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.calificacion = calificacion;
+	}
+	
+	
+
+}
---
+++ C2_Java_Basics/T9Herencia/src/Profesores.java
@@ -0,0 +1,19 @@
+
+public class Profesores {
+
+	protected String nombre;
+	protected int edad;
+	protected String sexo;
+    protected String materia;
+    
+	public Profesores(String nombre, int edad, String sexo, String materia) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.materia = materia;
+	}
+	
+    
+    
+
+}
