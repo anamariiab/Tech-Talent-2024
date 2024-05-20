90fd2b552ca06fe5827c227871737877c0ae13e0
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-05-20 23:15:16 +0200

    C4 Maven

---
+++ C4_Desarollo_Backend/src/main/java/com/joroba/ejemplo/C4_Desarollo_Backend/App.java
@@ -0,0 +1,13 @@
+package com.joroba.ejemplo.C4_Desarollo_Backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
