e9b5b596940f0ae05689530e6851df2a2f0a7aaf
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-05-21 10:29:31 +0200

    UD 20

---
+++ C4_Desarollo_Backend/src/main/java/UD20Maven/Ejercicio1/Ejercicio1App.java
@@ -0,0 +1,21 @@
+package UD20Maven.Ejercicio1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Ejercicio1App {
+	
+	public static void main(String[] args) {
+		// Configuración de la ventana principal
+		JFrame frame = new JFrame("Mi Aplicación GUI");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 300); // Tamaño inicial de la ventana
+
+		// Configuración de una etiqueta
+		JLabel label = new JLabel("¡Hola, Mundo!", SwingConstants.CENTER);
+		frame.getContentPane().add(label, BorderLayout.CENTER);
+
+		// Hacer visible la ventana
+		frame.setVisible(true);
+	}
+}
